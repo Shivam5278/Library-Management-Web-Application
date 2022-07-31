cd4dbbafb43eb3e41501772d0d16eee47bf21544
Shivam5278 <st527874@gmail.com>
2022-08-01 02:13:00 +0530

    Edits

--- datas.py
+++ datas.py
@@ -1,13 +1,16 @@
 import json
 import urllib.request, urllib.parse, urllib.error
 import ssl
+from urllib.parse import quote
+
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 title = input('Enter Name: ')
-url = 'https://frappe.io/api/method/frappe-library?title='+title
+
+url = 'https://frappe.io/api/method/frappe-library?title='+quote(title)
 print(url)
 fhand=urllib.request.urlopen(url)
 data=fhand.read().decode()
@@ -25,5 +28,7 @@ for item in info['message']:
 
 #bookid = input('Enter Reference ID: ')
 #a=bookidx.index(bookid)
-
+for i in titlex:
+    print(i)
 print(bookidx[1],':     ', titlex[1],'  Pages:',pages[1])
+#{"error":"Invalid Parameters","total_records":4000,"allowed_params":["title","authors","isbn","publisher","page"]}
