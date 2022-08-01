10711b146ff6e81c9b6a5398de015d27406e4661
Shivam5278 <st527874@gmail.com>
2022-08-02 03:46:28 +0530

    Add & delete book
    Add & delete book functionality added

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from app import db
 class Books(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
-    author = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(255))
     quantity = db.Column(db.Integer, nullable=False)
 
 
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from flask import render_template, url_for, flash, get_flashed_messages, redirect, request
+from flask import render_template, url_for, flash, session, request, get_flashed_messages, redirect, request
 from datetime import datetime
 
 import sys
@@ -8,7 +8,46 @@ import forms
 #from datas import data
 @app.route('/')
 @app.route('/index')
-@app.route('/home')
+@app.route('/home', methods = ['POST', 'GET'])
 def home():
     books = models.Books.query.all()
-    return render_template('home.html', books=books)
+    if request.method =="POST":
+
+        session["book_name"] = request.form["bknm"]
+        return redirect(url_for("booke"))
+    else:
+        return render_template('home.html', books=books)
+
+@app.route("/book")
+def booke():
+    if "book_name" in session:
+        book = session["book_name"]
+        return f"<h1>{book}</h1>"
+    else:
+        return redirect(url_for("home"))
+
+@app.route('/delete/<book_n>', methods=['GET', 'POST'])
+def delete(book_n):
+    form = forms.DeleteTaskForm()
+    bookn = models.Books.query.get(book_n)
+    if bookn:
+        if form.validate_on_submit():
+            if form.submit.data:
+                db.session.delete(bookn)
+                db.session.commit()
+                flash('book deleted')
+            return redirect(url_for('home'))
+        return render_template('delete.html', form=form, book_n=book_n, title=bookn)
+    flash(f'Task with id {book_n} does not exit')
+    return redirect(url_for('home'))
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = forms.AddTaskForm()
+    if form.validate_on_submit():
+        task = models.Books(title=form.title.data, author="a" ,quantity=5)
+        db.session.add(task)
+        db.session.commit()
+        flash('Book added')
+        return redirect(url_for('home'))
+    return render_template('add.html', form=form)
