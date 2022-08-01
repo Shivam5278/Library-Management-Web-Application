4e65400df455051736eaf46646fb784942e89f47
Shivam5278 <st527874@gmail.com>
2022-08-01 19:44:25 +0530

    update

--- routes.py
+++ routes.py
@@ -2,6 +2,7 @@ from app import app, db
 from flask import render_template, url_for, flash, get_flashed_messages, redirect, request
 from datetime import datetime
 
+import sys
 import models
 import forms
 #from datas import data
@@ -15,43 +16,12 @@ def index():
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    search = forms.AddTaskForm()
-    if search.validate_on_submit():
+    search = 'akmaks'#forms.AddTaskForm()
+    #if search.validate_on_submit():
 
-        flash('These are the books for your search')
-        return redirect(url_for('index'))
-    return render_template('search.html', search=search)
+    #    flash('These are the books for your search')
+    #    return redirect(url_for('index'))
+    return render_template('search.html', search=search), search
 
 x=search
-@app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
-def edit(task_id):
-    form = forms.AddTaskForm()
-    task = models.Task.query.get(task_id)
-    print(task)
-    if task:
-        if form.validate_on_submit():
-            task.title = form.title.data
-            task.date = datetime.utcnow()
-            db.session.commit()
-            flash('Task updated')
-            return redirect(url_for('index'))
-        form.title.data = task.title
-        return render_template('edit.html', form=form, task_id=task_id)
-    flash(f'Task with id {task_id} does not exit')
-    return redirect(url_for('index'))
-
-
-@app.route('/delete/<int:task_id>', methods=['GET', 'POST'])
-def delete(task_id):
-    form = forms.DeleteTaskForm()
-    task = models.Task.query.get(task_id)
-    if task:
-        if form.validate_on_submit():
-            if form.submit.data:
-                db.session.delete(task)
-                db.session.commit()
-                flash('Task deleted')
-            return redirect(url_for('index'))
-        return render_template('delete.html', form=form, task_id=task_id, title=task.title)
-    flash(f'Task with id {task_id} does not exit')
-    return redirect(url_for('index'))
+sys.modules[__name__] = search
